cb030aa359c9dfd59e7dd4dd80854f2b161c7f6f
Mai Sherif <65079937+MaiSheirf@users.noreply.github.com>
2021-02-12 23:43:48 +0200

    Update Co_code_inmips_final1.asm
    main updates

--- Co_code_inmips_final1.asm
+++ Co_code_inmips_final1.asm
@@ -6,10 +6,11 @@ massage2: .asciiz "number is not found  \n "
 .text 
 main:
   la $a0 ,myarray
-  addi $a1,$s0,5
-  jal Insertion_sort
-  jal binarySearch
-  #jal quick
+  addi $a1,$s0,0
+  addi $a2,$s1,4
+  #jal Insertion_sort
+  #jal binarySearch
+  jal quick
   addi $a0,$v0,0
   li $v0,1
   syscall
@@ -102,20 +103,18 @@ jr $ra
           
 ##############################################(quick)#################################
 # consider that list[i] = a0     first =a1   last=a2
-# i=s0 , t0  j=s1 , t1  pivot=s2 ,t2    temp=$t7
+# i= t0  j= t1  pivot= t2    temp=$t7
    quick:
       if:
-        slt  $t3 , $a0 ,$a1       
+        slt  $t3 , $a1 ,$a2       
     beq  $t3 ,$0 ,exif
-    add $t2 ,$s2 ,$a1        # pivot=first 
-    add $t0 ,$s0 ,$a1      # i = first 
-    add $t1 ,$s1 ,$a2                    # j = last 
+    addi $t2 ,$a1,0       # pivot=first 
+    addi $t0 ,$a1,0      # i = first 
+    addi $t1 ,$a2 ,0                    # j = last 
   while1:
     slt  $t4 , $t0 ,$t1                   # i<j 
     beq  $t4 ,$0 ,exwhile1          # if true t4=1 it will contaiue
-
-
-while2:
+  while2:
     sll $t4,$t0,2             #i*4
     add $t4,$t4 ,$a0          #list []+4 
     lw $t4,0($t4)      #list[i]
@@ -124,30 +123,33 @@ while2:
     lw $t5,0($t5)             #list[pivot]
     #check
     slt $t6,$t4,$t5                  # check if list[i]<=list[pivot]
-    bne $t6,$0,exwhile1
-    slt $t6,$a2 ,$t0                 # checking i<last
-    bne $t6,$0,exwhile1
+    beq $t6,$0,exwhile1
+    slt $t6 ,$t0,$a2               # checking i<last
+    beq $t6,$0,exwhile1
     addi $t0 ,$t0, 1
     j while2
   while3:
     sll $t6,$t1,2             #j*4
     add $t6,$t6 ,$a0          #list []+4 
     lw $t6,0($t6)             #   $t6=list[j]
-    slt $t7,$t6,$t5
+    sll $t5,$t2,2             #pivot*4
+    add $t5,$t5 ,$a0          #list []+4
+    lw $t5,0($t5)             #list[pivot]
+    slt $t7,$t5,$t6
     beq $t7,0,exwhile1
     addi $t1,$t1,-1
     j while3
   if2:
-        slt  $t3 , $a0 ,$t1       #if i<j
+        slt  $t3 , $t0 ,$t1       #if i<j
     beq  $t3 ,$0 ,exif
-    add $t7,$t7,$t4          #temp=list[i];
-    add $t4 ,$t4,$t6        #list[i]=list[j]
-    add $t6,$t6,$t7         #list[j]=temp
+    move $t7,$t4          #temp=list[i];
+    move $t4,$t6        #list[i]=list[j]
+    move $t6,$t7         #list[j]=temp
     j while1
   exwhile1:
-    add $t7,$t7,$t5  #temp=list[pivot]
-    add $t5,$t5,$t6      #list[pivot]=list[j]
-    add $t6,$t6,$t7        # list[j]=temp;
+    move $t7,$t5  #temp=list[pivot]
+    move $t5,$t6      #list[pivot]=list[j]
+    move $t6,$t7        # list[j]=temp;
     addi $a2,$t1,-1        #last = j-1
     jal  quick    
     addi $a1,$t1,1    #first =j+1
